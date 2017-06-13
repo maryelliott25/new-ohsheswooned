294ccaedd000e37dae85c1f11784bef5361e85c7
Mary Elliott <maryelliott@gmail.com>
2017-06-13 15:14:12 -0700

    add js tasks to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 var autoprefixer = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var jshint = require('gulp-jshint');
 var livereload = require('gulp-livereload');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
 
 //Copy PHP Files
 gulp.task('php', function() {
@@ -46,6 +49,25 @@ gulp.task('prod-scss', function() {
 	.pipe(gulp.dest('dest'))
 });
 
+//Dev JS
+gulp.task('dev-js', function() {
+	gulp.src('src/assets/js/**/*.js')
+	.pipe(sourcemaps.init())
+	.pipe(jshint())
+	.pipe(jshint.reporter('jshint-stylish'))
+	.pipe(concat('main.js'))
+	.pipe(sourcemaps.write())
+	.pipe(gulp.dest('dest/assets/js'))
+});
+
+//Prod JS
+gulp.task('prod-js', function() {
+	gulp.src('src/assets/js/**/*.js')
+	.pipe(concat('main.js'))
+	.pipe(uglify())
+	.pipe(gulp.dest('dest/assets/js'))
+});
+
 //Log
 gulp.task('log', function() {
 	gutil.log('== My Log Task ==')
@@ -57,8 +79,9 @@ gulp.task('watch', function() {
 	gulp.watch('src/**/*.php', ['php']);
 	gulp.watch('src/**/*.{png,gif,jpg,jpeg}', ['images']);
 	gulp.watch('src/assets/scss/**/*.scss', ['dev-scss']);
+	gulp.watch('src/assets/js/**/*.js', ['dev-js']);
 });
 
-gulp.task('dev', ['php', 'images', 'dev-scss', 'watch']);
+gulp.task('dev', ['php', 'images', 'dev-scss', 'dev-js', 'watch']);
 
-gulp.task('prod', ['php', 'images', 'prod-scss']);
+gulp.task('prod', ['php', 'images', 'prod-scss', 'prod-js']);
